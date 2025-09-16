2c6c404b75622aeb6bf88391eb83c2118ab71dad
mariarobin895@gmail.com <mariarobin895@gmail.com>
2025-09-16 19:16:42 +0530

    commit form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import logo from './logo.svg';
+import {useEffect, useState} from "react";
 import './App.css';
 
 function App() {
+
+  const[username,setUsername]=useState("");
+  const[lastName,setlastName]=useState("");
+
+
+
+   const savefirstname=(event)=>{
+
+    setUsername(event.target.value);
+
+   }
+
+   const savelastname=(event)=>{
+
+    setlastName(event.target.value);
+
+   }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <form id="PersonalDetail" name="PersonalDetail">
+      <div className="Name" style={{display:"flex",flexDirection:"column", gap:"10px"}}>
+
+<label htmlfor="firstName" >
+<input id="firstName" type="text" name="firstName" placeholder="FirstName" value={username} onChange={(event)=>(savefirstname(event))} required></input>
+</label>
+
+<label htmlfor="lastName" >
+<input id="lastName" type="text" name="lastName" placeholder="LastName" value={lastName} onChange={(event)=>(savelastname(event))} required></input>
+</label>
+
+ <div className="button"> 
+<button type="submit">Button</button>
+<div>Full Name:{username} {lastName}</div>
+ </div>
+</div>
+    </form>
+  
   );
 }
 
